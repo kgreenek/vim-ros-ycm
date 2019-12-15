455d945d0e66b536d66c21bae9723e28224d921d
Kevin Greene <kgreenek@gmail.com>
2019-12-14 22:25:04 -0800

    Add catkin_config.sh helper script

---
+++ catkin_config.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -e
+dir=$(cd $(dirname "${BASH_SOURCE[0]:-$0}") && pwd)
+ros_workspace=$(catkin locate)
+catkin config --cmake-args="-DCMAKE_EXPORT_COMPILE_COMMANDS=ON" $@
+cp -f "${dir}/.ycm_extra_conf.py" "${ros_workspace}/.ycm_extra_conf.py"
