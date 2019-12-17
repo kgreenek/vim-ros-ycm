5de224b1216a51d5bf9a3c98ebff7dc65454a39b
Kevin Greene <kgreenek@gmail.com>
2019-12-17 13:19:57 -0800

    Fix cmake args in catkin_config.sh

--- catkin_config.sh
+++ catkin_config.sh
@@ -2,5 +2,5 @@
 set -e
 dir=$(cd $(dirname "${BASH_SOURCE[0]:-$0}") && pwd)
 ros_workspace=$(catkin locate)
-catkin config --cmake-args="-DCMAKE_EXPORT_COMPILE_COMMANDS=ON" $@
+catkin config $@ --cmake-args "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON"
 cp -f "${dir}/.ycm_extra_conf.py" "${ros_workspace}/.ycm_extra_conf.py"
